14d3af86bd3ff6540e9f7e37cc610cbb3e6e460d
Linda Andersson <linda.29.andersson@gmail.com>
2022-09-21 13:15:06 +0200

    Added my solutions folder and first js-recap-easy age solution

---
+++ Lindas-Solutions/js-recap/js-recap-easy.js
@@ -0,0 +1,12 @@
+// Age
+
+const object = { name: "LA", age: 29 }
+
+function logAge(o) {
+    if (o.age) return console.log(o.age);
+    return console.log("No age data");
+}
+
+console.log(logAge(object));
+
+// Ages
